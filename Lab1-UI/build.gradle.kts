6ac08dc706b8f6d3ea7a08cb2f484b7d08db0150
DanielJimenez0429 <danieljimenez200429@gmail.com>
2026-09-03 20:31:23 -0500

    Change minSdk version from 26 to 23

--- Lab1-UI/build.gradle.kts
+++ Lab1-UI/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "co.edu.udea.compumovil.gr07_20262.lab1"
-        minSdk = 26
+        minSdk = 23
         targetSdk = 37
         versionCode = 1
         versionName = "1.0"
