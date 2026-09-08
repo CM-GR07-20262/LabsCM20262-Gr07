985278daf92c049632a315005b3b4a5989505e61
brayan <brayanjaraba2006@gmail.com>
2026-09-08 16:13:22 -0500

    chore: optimize imports

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr07_20262/lab1/ui/theme/components/personaldata/SexoSelector.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr07_20262/lab1/ui/theme/components/personaldata/SexoSelector.kt
@@ -1,7 +1,6 @@
 package co.edu.udea.compumovil.gr07_20262.lab1.ui.theme.components.personaldata
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
