fa1f8ec2719b57b48005a48a9f958806a18b3809
brayan <brayanjaraba2006@gmail.com>
2026-09-08 16:34:30 -0500

    feat: make GradoEscolaridadSelector.kt has a leading icon and extract string resources

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr07_20262/lab1/ui/theme/components/personaldata/GradoEscolaridadSelector.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr07_20262/lab1/ui/theme/components/personaldata/GradoEscolaridadSelector.kt
@@ -3,6 +3,8 @@ package co.edu.udea.compumovil.gr07_20262.lab1.ui.theme.components.personaldata
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -11,11 +13,20 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import co.edu.udea.compumovil.gr07_20262.lab1.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GradoEscolaridadSelector(seleccionado: String, onSeleccionChange: (String) -> Unit) {
-  val opciones = listOf("Primaria", "Secundaria", "Universitaria", "Otro")
+  val opciones = listOf(
+    stringResource(R.string.primary),
+    stringResource(R.string.secondary),
+    stringResource(R.string.university),
+    stringResource(R.string.other)
+  )
   var expanded by remember { mutableStateOf(false) }
 
   ExposedDropdownMenuBox(
@@ -26,8 +37,15 @@ fun GradoEscolaridadSelector(seleccionado: String, onSeleccionChange: (String) -
       value = seleccionado,
       onValueChange = {},
       readOnly = true,
-      label = { Text("Grado de escolaridad") },
-      modifier = Modifier.menuAnchor()
+      label = { Text(stringResource(R.string.school_level)) },
+      modifier = Modifier.menuAnchor(),
+      leadingIcon = {
+        Icon(
+          painterResource(R.drawable.school),
+          "School icon",
+          tint = MaterialTheme.colorScheme.primary
+        )
+      }
     )
     ExposedDropdownMenu(
       expanded = expanded,
@@ -44,4 +62,10 @@ fun GradoEscolaridadSelector(seleccionado: String, onSeleccionChange: (String) -
       }
     }
   }
+}
+
+@Preview
+@Composable
+private fun PreviewGradoEscolaridadSelector() {
+  GradoEscolaridadSelector("") { }
 }
