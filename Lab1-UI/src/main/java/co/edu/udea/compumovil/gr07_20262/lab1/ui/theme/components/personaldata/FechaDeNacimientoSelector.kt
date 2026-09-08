2aaea30cacfcb35b3a4f083a5f36ef36a9d5968d
brayan <brayanjaraba2006@gmail.com>
2026-09-08 16:25:44 -0500

    feat: make FechaDeNacimientoSelector.kt component consistent with the rest of components, add a leading icon and extract strings resources

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr07_20262/lab1/ui/theme/components/personaldata/FechaDeNacimientoSelector.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr07_20262/lab1/ui/theme/components/personaldata/FechaDeNacimientoSelector.kt
@@ -5,19 +5,28 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.DatePickerState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import co.edu.udea.compumovil.gr07_20262.lab1.R
+import co.edu.udea.compumovil.gr07_20262.lab1.ui.theme.Labs20262Gr07Theme
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 import java.util.TimeZone
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FechaNacimientoSelector(
   fechaNacimiento: String,
@@ -28,19 +37,24 @@ fun FechaNacimientoSelector(
   onCloseDatePicker: () -> Unit,
   onSetFechaDeNacimiento: (String) -> Unit
 ) {
-  OutlinedTextField(
+  TextField(
     value = fechaNacimiento,
     onValueChange = {},
-    label = { Text("Fecha de nacimiento *") },
+    label = { Text(stringResource(R.string.brith_day)) },
     modifier = modifier
       .fillMaxWidth()
       .clickable { onFechaClick() },
-    enabled = false,    // Desabilitamos la escritura directa para obligar a utilizar el Picker
-    colors = OutlinedTextFieldDefaults.colors(
+    enabled = false,    // Deshabilitamos la escritura directa para obligar a utilizar el Picker
+    colors = TextFieldDefaults.colors(
       disabledTextColor = MaterialTheme.colorScheme.onSurface,
-      disabledBorderColor = MaterialTheme.colorScheme.outline,
       disabledLabelColor = MaterialTheme.colorScheme.onSurfaceVariant
-    )
+    ),
+    leadingIcon = {
+      Icon(
+        painterResource(R.drawable.calendar), "Calendar Icon",
+        tint = MaterialTheme.colorScheme.primary
+      )
+    }
   )
 
   // Lógica para diálogo flotante (Se visualiza solo si mostrarDatePicker es true)
@@ -59,17 +73,34 @@ fun FechaNacimientoSelector(
           }
           onCloseDatePicker()
         }) {
-          Text("Aceptar")
+          Text(stringResource(R.string.accept))
         }
       },
       dismissButton = {
         TextButton(onClick = onCloseDatePicker) {
-          Text("Cancelar")
+          Text(stringResource(R.string.cancel))
         }
       }
     ) {
       DatePicker(state = datePickerState)
     }
+  }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun FechaNacimientoSelectorPreview() {
+  Labs20262Gr07Theme {
+    val datePickerState = rememberDatePickerState()
+    FechaNacimientoSelector(
+      fechaNacimiento = "",
+      onFechaClick = {},
+      mostrarDatePicker = false,
+      datePickerState = datePickerState,
+      onCloseDatePicker = {},
+      onSetFechaDeNacimiento = {}
+    )
   }
 }
 
