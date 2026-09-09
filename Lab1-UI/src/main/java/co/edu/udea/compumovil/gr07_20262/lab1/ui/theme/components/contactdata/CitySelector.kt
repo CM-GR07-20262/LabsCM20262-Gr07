8c6c74c1e6e237d226ad1d2d621b3377fe9c700f
brayan <brayanjaraba2006@gmail.com>
2026-09-08 22:14:43 -0500

    feat: create city selector

---
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr07_20262/lab1/ui/theme/components/contactdata/CitySelector.kt
@@ -0,0 +1,123 @@
+package co.edu.udea.compumovil.gr07_20262.lab1.ui.theme.components.contactdata
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuAnchorType
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import co.edu.udea.compumovil.gr07_20262.lab1.R
+import java.util.Locale.getDefault
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CitySelector(cities: List<String>, onSelectCity: (String) -> Unit) {
+  var expanded by remember { mutableStateOf(false) }
+  var citiesFilter by remember { mutableStateOf("") }
+  ExposedDropdownMenuBox(
+    expanded = expanded,
+    onExpandedChange = { expanded = !expanded }
+  ) {
+    TextField(
+      value = citiesFilter,
+      onValueChange = {
+        citiesFilter = it
+        expanded = true
+      },
+      label = { Text(stringResource(R.string.city)) },
+      leadingIcon = {
+        Icon(
+          painterResource(R.drawable.building), "Building icon",
+          tint = MaterialTheme.colorScheme.primary
+        )
+      },
+      maxLines = 1,
+      singleLine = true,
+      modifier = Modifier.menuAnchor(ExposedDropdownMenuAnchorType.PrimaryEditable)
+    )
+
+    val filteredCities = cities.filter {
+      it.lowercase(getDefault()).contains(citiesFilter.lowercase(getDefault()))
+    }
+    ExposedDropdownMenu(
+      expanded = expanded,
+      onDismissRequest = { expanded = false }
+    ) {
+      filteredCities.forEach {
+        DropdownMenuItem(
+          text = { Text(it) },
+          onClick = {
+            citiesFilter = it
+            onSelectCity(it)
+            expanded = false
+          }
+        )
+
+      }
+    }
+  }
+}
+
+
+@Preview
+@Composable
+private fun PreviewCitySelector() {
+  val cities = listOf(
+    "Bogotá",
+    "Medellín",
+    "Cali",
+    "Barranquilla",
+    "Cartagena",
+    "Cúcuta",
+    "Bucaramanga",
+    "Pereira",
+    "Santa Marta",
+    "Ibagué",
+    "Manizales",
+    "Villavicencio",
+    "Pasto",
+    "Montería",
+    "Neiva",
+    "Armenia",
+    "Popayán",
+    "Sincelejo",
+    "Valledupar",
+    "Tunja",
+    "Riohacha",
+    "Quibdó",
+    "Florencia",
+    "Yopal",
+    "Buenaventura",
+    "Palmira",
+    "Soacha",
+    "Apartadó",
+    "Turbo",
+    "Carepa"
+  )
+  var city by remember { mutableStateOf("") }
+  val onSelectCity: (String) -> Unit = { city = it }
+
+  Column(
+    Modifier.fillMaxSize(),
+    verticalArrangement = Arrangement.Center,
+    horizontalAlignment = Alignment.CenterHorizontally
+  ) {
+    CitySelector(cities, onSelectCity)
+  }
+
+}
