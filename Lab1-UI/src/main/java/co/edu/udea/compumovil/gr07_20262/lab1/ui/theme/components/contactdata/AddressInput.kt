0a5d1a4e21450577806d6c4b00825a15dd584c82
brayan <brayanjaraba2006@gmail.com>
2026-09-08 22:32:25 -0500

    feat: implement address input component

---
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr07_20262/lab1/ui/theme/components/contactdata/AddressInput.kt
@@ -0,0 +1,66 @@
+package co.edu.udea.compumovil.gr07_20262.lab1.ui.theme.components.contactdata
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import co.edu.udea.compumovil.gr07_20262.lab1.R
+import co.edu.udea.compumovil.gr07_20262.lab1.ui.theme.Labs20262Gr07Theme
+
+@Composable
+fun AddressInput(addressValue: String, onAddressChange: (String) -> Unit) {
+
+  val addressRegex = Regex(
+    """^(Calle|Cra?\.?|Carrera|Cl\.?|Transversal|Tv\.?|Diagonal|Dg\.?)\s+\d+[A-Za-z]?\s*#\s*\d+[A-Za-z]?\s*-\s*\d+$"""
+  )
+  TextField(
+    value = addressValue,
+    onValueChange = onAddressChange,
+    isError = addressValue.matches(addressRegex),
+    leadingIcon = {
+      Icon(
+        painterResource(R.drawable.address),
+        "Address icon",
+        tint = MaterialTheme.colorScheme.primary
+      )
+    },
+    colors = TextFieldDefaults.colors(
+      errorTextColor = MaterialTheme.colorScheme.error,
+      errorLabelColor = MaterialTheme.colorScheme.error,
+      errorContainerColor = MaterialTheme.colorScheme.errorContainer
+    ),
+    maxLines = 1,
+    singleLine = true,
+    label = { Text(stringResource(R.string.address)) }
+  )
+}
+
+
+@Preview
+@Composable
+private fun PreviewAddressInput() {
+  var address by remember { mutableStateOf("") }
+  Labs20262Gr07Theme {
+    Column(
+      Modifier.fillMaxSize(),
+      horizontalAlignment = Alignment.CenterHorizontally,
+      verticalArrangement = Arrangement.Center
+    ) {
+      AddressInput(address) { address = it }
+    }
+  }
+}
